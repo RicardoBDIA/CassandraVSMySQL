7702775c39e2aebcbcd29541596e61034aa6d22c
RicardoBDIA <ricardovilarinobdia@gmail.com>
2024-11-15 17:11:34 +0100

    New dataset

--- dataset.py
+++ dataset.py
@@ -1,8 +1,9 @@
 import csv
 from faker import Faker
 import random
+from tqdm import tqdm
 
-NUM_RECORDS = 10
+NUM_RECORDS = 1000000
 OUTPUT_FILE = 'data.csv'
 
 fake = Faker()
@@ -13,15 +14,17 @@ with open(OUTPUT_FILE, mode='w', newline='') as file:
     writer.writerow(["name", "email", "age"])  # Header
     
     count = 0
-    while count < NUM_RECORDS:
-        email = fake.email()
-        if email not in emails:  # Verifica si el email ya existe
-            emails.add(email)  # Añade el email al conjunto para evitar duplicados
-            writer.writerow([
-                fake.name(),
-                email,
-                random.randint(0, 99)
-            ])
-            count += 1
+    with tqdm(total=NUM_RECORDS, desc="Generando datos", unit="registros") as pbar:
+        while count < NUM_RECORDS:
+            email = fake.email()
+            if email not in emails:  # Verifica si el email ya existe
+                emails.add(email)
+                writer.writerow([
+                    fake.name(),
+                    email,
+                    random.randint(0, 99)
+                ])
+                count += 1
+                pbar.update(1)  # Actualiza la barra de progreso
 
 print(f'{NUM_RECORDS} registros únicos generados en el archivo {OUTPUT_FILE}.')
